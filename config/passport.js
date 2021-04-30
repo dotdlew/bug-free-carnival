ba756d20476e75822b2067e03438c7a0b0f7716e
Daniel Lewis <daniel.lewis@protonmail.com>
2021-04-29 20:51:12 -0600

    added User model, added carnival_db

---
+++ config/passport.js
@@ -0,0 +1,36 @@
+LocalStrategy = require('passport-local').Strategy;
+
+const bcrypt = require('bcryptjs');
+
+//Import model here ....bcrypt
+
+
+//export function (passport) {
+    //passport.use(new LocalStrategy(
+//   function(username, password, done) {
+//     User.findOne({ username: username }, function (err, user) {
+//       if (err) { return done(err); }
+//       if (!user) {
+//         return done(null, false, { message: 'Incorrect username.' });
+//       }//
+//          bcrypt compare
+//       if (!user.validPassword(password)) {
+//         return done(null, false, { message: 'Incorrect password.' });
+//       }
+//       return done(null, user);
+//     });
+//   }
+// passport.serializeUser(function(user, done) {
+//     done(null, user.id);
+//   });
+
+//   passport.deserializeUser(function(id, done) {
+//     User.findById(id, function(err, user) {
+//       done(err, user);
+//     });
+//   });
+// ));
+// 
+
+
+
