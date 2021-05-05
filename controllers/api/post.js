07a4f4b7e11aa834ca71ed6e32a22884d230e04b
Ryan Helm <rjhelm@github.io>
2021-05-04 20:47:45 -0600

    added routes CRUD for posts

--- controllers/api/post.js
+++ controllers/api/post.js
@@ -24,18 +24,76 @@ router.get('/', (req, res) => {
       },
     ],
     }).then((dbPostData) => res.json(dbPostData.reverse()))
-    .catch(err => {
+    .catch((err => {
+        console.log(err);
+        res.status(500).json(err);
+    }));
+});
+
+router.get('/:id', (req, res) => {
+    Post.findOne({
+        where: {
+            id: req.params.id,
+        },
+        attributes: ["id", "content", "title", "created_at"],
+        include: [
+            {
+                model: User,
+                attributes: ['username'],
+            },
+            {
+                model: Comment,
+                attributes: ["id", "comment_text", "post_id", "user_id", "created_at"],
+                include: {
+                    model: User,
+                    attributes: ['username'],
+                },
+            },
+        ],
+    }).then((dbPostData) => {
+        if (!dbPostData) {
+            req.status(404).json({ message: 'No post found with that id.' });
+            return;
+        }
+        res.json(dbPostData);
+    }).catch((err) => {
+        console.log(err);
+        res.status(500).json(err);
+    })
+})
+
+router.post('/', (req, res) => {
+    Post.create({
+        title: req.body.title,
+        content: req.body.content,
+        user_id: req.session.user_id,
+    }).then((dbPostData) => res.json(dbPostData))
+    .catch((err) => {
         console.log(err);
         res.status(500).json(err);
     });
 });
 
-router.get('/:id', (req, res) => {
-    
-})
-
-router.post('/', (req, res) => {
-    Post.create({
-        
-    })
+router.put('/:id', (req, res) => {
+    Post.update(
+        {
+            title: req.body.title,
+            content: req.body.content,
+        },
+        {
+            where: {
+                id: req.params.id,
+            }
+        }
+    ).then((dbPostData) => {
+        if (!dbPostData) {
+            res.status(404).json({ message: 'No post was found with this id.' });
+            return;
+        }
+        res.json(dbPostData);
+    }).catch((err) => {
+        console.log(err);
+        res.status(500).json(err);
+    });
 });
+
