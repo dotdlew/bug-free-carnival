84125bc016c2849918097f4e4ffb95c69cd5eeba
javiddelossantos5 <javiddelossantos5@gmail.com>
2021-05-05 00:23:29 -0600

    added a register and a login handlebar 1 is for logging in 1 is register auto redirects to /login after making registering

--- carnival.js
+++ carnival.js
@@ -22,15 +22,16 @@ initializePassport(
     id => users.find(user => user.id === id)
 );
 
+
 app.engine('handlebars', hbs.engine);
 app.set('view-engine', 'handlebars');
 
 app.use(express.urlencoded({ extended: false }));
 app.use(flash())
 app.use(session({
-    secret: process.env.SESSION_SECRET,
-    resave: false,
-    saveUninitialized: false
+  secret: process.env.SESSION_SECRET,
+  resave: false,
+  saveUninitialized: false
 }))
 
 app.use(passport.initialize())
@@ -38,6 +39,7 @@ app.use(passport.session())
 app.use(methodOverride('_method'))
 app.use(express.static(path.join(__dirname, 'public')));
 
+const users = [];
 
 // Connect to database
 const db = mysql.createConnection(
@@ -148,27 +150,27 @@ app.listen(PORT, () => {
 
 //for login 
 
-app.get('/', checkAuthenticated,  (req, res) => {
+app.get('/',  (req, res) => {
   res.render('homepage.handlebars')
 })
 
-// app.get('/login', checkNotAuthenticated, (req, res) => {
-//   res.render('./public/login.handlebars')
-// })
 app.get('/login', (req, res) => {
-  console.log('Testing')
+  res.render('login.handlebars')
+})
+
+app.get('/login', (req, res) => {
   res.render('login.handlebars')
 })
 
 app.post('/login', async (req, res) => {
   try {
       hashedPassword = await bcrypt.hash(req.body.password, 10)
-      // users.push({
-      //     id: Date.now().toString(),
-      //     name: req.body.name,
-      //     email: req.body.email,
-      //     password: hashedPassword
-      // })
+      users.push({
+          id: Date.now().toString(),
+          name: req.body.name,
+          email: req.body.email,
+          password: hashedPassword
+      })
       res.redirect('/login')
   } catch {
       res.redirect('/login')
@@ -176,27 +178,46 @@ app.post('/login', async (req, res) => {
   console.log(users);
 })
 
-app.post('/login', checkNotAuthenticated, passport.authenticate('local', {
+app.post('/login', passport.authenticate('local', {
   successRedirect: '/',
   failureRedirect: '/login',
   failureFlash: true
 }))
+
+app.get('/register', (req, res) => {
+  res.render('register.handlebars')
+})
+
+app.post('/register', async (req, res) => {
+  try {
+    const hashedPassword = await bcrypt.hash(req.body.password, 10)
+    users.push({
+      id: Date.now().toString(),
+      name: req.body.name,
+      email: req.body.email,
+      password: hashedPassword
+    })
+    res.redirect('/login')
+  } catch {
+    res.redirect('/register')
+  }
+})
 
 app.delete('/logout', (req, res) => {
   req.logOut()
   res.redirect('/login')
 })
 
-function checkAuthenticated(req, res, next) {
-  if (req.isAuthenticated()) {
-      return next()
-  }
-  res.redirect('/login')
-}
+// function checkAuthenticated(req, res, next) {
+//   if (req.isAuthenticated()) {
+//       return next()
+//   }
+//   res.redirect('/login')
+// }
 
-function checkNotAuthenticated(req, res, next) {
-  if (req.isAuthenticated()) {
-      return res.redirect('/')
-  }
-  next()
-}
+// function checkNotAuthenticated(req, res, next) {
+//   if (req.isAuthenticated()) {
+//       return res.redirect('/')
+//   }
+//   next()
+// }
