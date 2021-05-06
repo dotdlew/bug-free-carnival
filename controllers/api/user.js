414e3b3319e90d8fefb7787a92c563f9258ff330
Ryan Helm <rjhelm@github.io>
2021-05-06 14:47:31 -0600

    user routes done

--- controllers/api/user.js
+++ controllers/api/user.js
@@ -71,3 +71,67 @@ router.post('/', (req, res) => {
     });
 });
 
+router.post('/login', (req, res) => {
+    User.findOne({
+        where: {
+            username: req.body.username,
+        },
+    }).then((dbUserData) => {
+        if (!dbUserData) {
+            res.status(400).json({ message: 'No user was found with that username!' });
+            return; 
+        }
+        const validPassword = dbUserData.checkPassword(req.body.password);
+        if (!validPassword) {
+            res.status(400).json({ message: 'Incorrect Password!' });
+            return;
+        }
+        req.session.save(() => {
+            req.session.user_id = dbUserData.id;
+            req.session.username = dbUserData.username;
+            req.session.loggedIn = true;
+
+            res.json({ user: dbUserData, message: 'You are now logged in!' });
+        });
+        }).catch((err) => {
+            console.log(err);
+            res.status(500).json(err);
+    });
+});
+
+router.put('/:id', (req, res) => {
+    User.update(req.body, {
+        individualHooks: true,
+        where: {
+            id: req.params.id,
+        },
+    }).then((dbUserData) => {
+        if (!dbUserData[0]) {
+            res.status(404).json({ message: 'No user found with this id!' });
+            return;
+        }
+        res.json(dbUserData);
+    }).catch((err) => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
+router.delete("/:id", (req, res) => {
+    User.destroy({
+    where: {
+        id: req.params.id,
+    },
+    }).then((dbUserData) => {
+        if (!dbUserData) {
+            res.status(404).json({ message: "No user found with this id" });
+            return;
+        }
+            res.json(dbUserData);
+    }).catch((err) => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
+module.exports = router;
