1039443a8015dc43ffd07d547cc07e6b480bafa4
Daniel Lewis <daniel.lewis@protonmail.com>
2021-05-05 20:59:10 -0600

    formatting

--- carnival.js
+++ carnival.js
@@ -1,43 +1,43 @@
 require("dotenv").config();
 const inputCheck = require("./utils/inputCheck");
+const routes = require("./controllers");
 const express = require("express");
 const mysql = require("mysql2");
 const app = express();
-const path = require('path');
-// const bcrypt = require('bcrypt');
-const passport =  require('passport');
-const flash = require('express-flash');
-const session = require('express-session');
-const exphbs = require('express-handlebars');
-const helpers = require('./utils/helpers');
-const hbs = exphbs.create({helpers});
-const methodOverride = require('method-override');
+const path = require("path");
+// const bcrypt = require("bcrypt");
+const passport = require("passport");
+const flash = require("express-flash");
+const session = require("express-session");
+const exphbs = require("express-handlebars");
+const hbs = exphbs.create({ helpers });
+const helpers = require("./utils/helpers");
+const methodOverride = require("method-override");
 
 const PORT = process.env.PORT || 3001;
 
-const initializePassport = require('./config/passport');
+const initializePassport = require("./config/passport");
 initializePassport(
-    passport,
-    email =>  users.find(user => user.email === email),
-    id => users.find(user => user.id === id)
+  passport,
+  (email) => users.find((user) => user.email === email),
+  (id) => users.find((user) => user.id === id)
 );
 
-app.engine('handlebars', hbs.engine);
-app.set('view-engine', 'handlebars');
+app.engine("handlebars", hbs.engine);
+app.set("view-engine", "handlebars");
 
 app.use(express.urlencoded({ extended: false }));
-app.use(flash())
-app.use(session({
-    secret: process.env.SESSION_SECRET,
-    resave: false,
-    saveUninitialized: false
-}))
+app.use(flash());
+// app.use(session({
+//     secret: process.env.SESSION_SECRET,
+//     resave: false,
+//     saveUninitialized: false
+// }))
 
-app.use(passport.initialize())
-app.use(passport.session())
-app.use(methodOverride('_method'))
-app.use(express.static(path.join(__dirname, 'public')));
-
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(methodOverride("_method"));
+app.use(express.static(path.join(__dirname, "public")));
 
 // Connect to database
 const db = mysql.createConnection(
@@ -146,57 +146,61 @@ app.listen(PORT, () => {
   console.log(`SERVER RUNNING ON ${PORT}`);
 });
 
-//for login 
+//for login
 
-app.get('/', checkAuthenticated,  (req, res) => {
-  res.render('homepage.handlebars')
-})
+app.get("/", checkAuthenticated, (req, res) => {
+  res.render("homepage.handlebars");
+});
 
 // app.get('/login', checkNotAuthenticated, (req, res) => {
 //   res.render('./public/login.handlebars')
 // })
-app.get('/login', (req, res) => {
-  console.log('Testing')
-  res.render('login.handlebars')
-})
+app.get("/login", (req, res) => {
+  console.log("Testing");
+  res.render("login.handlebars");
+});
 
-app.post('/login', async (req, res) => {
+app.post("/login", async (req, res) => {
   try {
-      hashedPassword = await bcrypt.hash(req.body.password, 10)
-      // users.push({
-      //     id: Date.now().toString(),
-      //     name: req.body.name,
-      //     email: req.body.email,
-      //     password: hashedPassword
-      // })
-      res.redirect('/login')
+    hashedPassword = await bcrypt.hash(req.body.password, 10);
+    // users.push({
+    //     id: Date.now().toString(),
+    //     name: req.body.name,
+    //     email: req.body.email,
+    //     password: hashedPassword
+    // })
+    res.redirect("/login");
   } catch {
-      res.redirect('/login')
+    res.redirect("/login");
   }
   console.log(users);
-})
+});
 
-app.post('/login', checkNotAuthenticated, passport.authenticate('local', {
-  successRedirect: '/',
-  failureRedirect: '/login',
-  failureFlash: true
-}))
+app.post(
+  "/login",
+  checkNotAuthenticated,
+  passport.authenticate("local", {
+    successRedirect: "/",
+    failureRedirect: "/login",
+    failureFlash: true,
+  })
+);
 
-app.delete('/logout', (req, res) => {
-  req.logOut()
-  res.redirect('/login')
-})
+app.delete("/logout", (req, res) => {
+  req.logOut();
+  res.redirect("/login");
+});
 
 function checkAuthenticated(req, res, next) {
   if (req.isAuthenticated()) {
-      return next()
+    return next();
   }
-  res.redirect('/login')
+  res.redirect("/login");
 }
 
 function checkNotAuthenticated(req, res, next) {
   if (req.isAuthenticated()) {
-      return res.redirect('/')
+    return res.redirect("/");
   }
-  next()
+  next();
 }
